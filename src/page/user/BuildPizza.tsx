7d36fdeb7eae2f1be8f2f14f6c6c1ca01fd040b9
AkritiGhosh <akritighosh98@gmail.com>
2025-02-06 11:46:31 +0530

    build page UI - 1

--- src/page/user/BuildPizza.tsx
+++ src/page/user/BuildPizza.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import PageLayout from "../../components/common/PageLayout";
 
 const BuildPizza = () => {
   return (
-    <div>BuildPizza</div>
-  )
-}
+    <PageLayout>
+      <main
+        className={`w-screen h-screen inset-0 bg-cover bg-right bg-fixed overflow-auto bg-[url("/background.jpg")] px-4 xl:px-[5%] pt-28 pb-5`}
+      >
+        <div className="w-full h-auto min-h-[50%] overflow-y-auto rounded dark:bg-black/90 bg-white/90 blur-lg">
+          <div className="flex justify-between gap-5">
+            <div className="w-1/2 p-4"></div>
+            <div className="w-1/2 p-4"></div>
+          </div>
+        </div>
+      </main>
+    </PageLayout>
+  );
+};
 
-export default BuildPizza
+export default BuildPizza;
