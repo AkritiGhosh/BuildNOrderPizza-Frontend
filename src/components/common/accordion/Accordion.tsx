b3f219a132786292c236248525d1f8983c335082
AkritiGhosh <akritighosh98@gmail.com>
2025-02-07 18:41:14 +0530

    accordion - UI

--- src/components/common/accordion/Accordion.tsx
+++ src/components/common/accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 
 const Accordion = ({
   children,
@@ -7,7 +7,45 @@ const Accordion = ({
   children: ReactNode;
   title: String;
 }) => {
-  return <div>Accordion</div>;
+  const [open, setOpen] = useState(false);
+
+  const toggleAccordion = (e: React.MouseEvent<Element, MouseEvent>): void =>
+    setOpen((prev) => !prev);
+  return (
+    <div className="w-full relative my-2">
+      <button
+        onClick={toggleAccordion}
+        className={`w-full h-12 px-2 py-1 flex items-center justify-between text-base ${
+          open
+            ? "text-amber-600 font-semibold bg-black/70 rounded-t-md"
+            : "text-amber-300 font-normal bg-black/30 rounded-md"
+        } transition-colors duration-300`}
+      >
+        <span>{title}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={open ? 3 : 2}
+          stroke="currentColor"
+          className={`size-4 ${
+            open ? "-scale-y-100" : "scale-y-100"
+          } transition-transform duration-300`}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m11.99 16.5 3.75 3.75m0 0 3.75-3.75m-3.75 3.75V3.75H4.49"
+          />
+        </svg>
+      </button>
+      {open ? (
+        <div className="bg-black/50 p-2 rounded-b-md grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-4">
+          {children}
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Accordion;
--- src/components/user/build-pizza/PizzaComponents.tsx
+++ src/components/user/build-pizza/PizzaComponents.tsx
@@ -20,16 +20,34 @@ const PizzaComponents = () => {
         },
       ],
     },
+    {
+      component: "Veg",
+      data: [
+        {
+          name: "Onion",
+          price: 20,
+          isAvailable: true,
+        },
+        {
+          name: "Mushroom",
+          price: 30,
+          isAvailable: true,
+        },
+      ],
+    },
   ];
   return (
     <>
       <h1>Choose your pizza selection</h1>
-      {components?.map((component) => (
-        <Accordion title={component?.component}>
-          {component?.data?.map((option) => (
-            <div>{option.name}</div>
-          ))}
-        </Accordion>
+      {components?.map((component, x) => (
+        <>
+          <Accordion title={component?.component}>
+            {component?.data?.map((option) => (
+              <div>{option.name}</div>
+            ))}
+          </Accordion>
+          {x != components?.length - 1 && <hr className="border-amber-500" />}
+        </>
       ))}
     </>
   );
