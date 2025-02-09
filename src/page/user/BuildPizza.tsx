4e33e57ffac45e90df5851dc73bfc6410ee05925
AkritiGhosh <akritighosh98@gmail.com>
2025-02-09 21:02:31 +0530

    UI updated

--- src/page/user/BuildPizza.tsx
+++ src/page/user/BuildPizza.tsx
@@ -4,17 +4,25 @@ import PizzaCart from "../../components/user/build-pizza/PizzaCart";
 import { useState } from "react";
 
 const BuildPizza = () => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState<object>([]);
+  // const [pizzaSelected, SetPizzaSelected] = useState(0);
+
+  // const addItemToCart = (item) => {};
+  // const removeItemFromCart = (item) => {};
+  // const changeQuantity = (item, quantity) => {};
+
   return (
     <PageLayout>
       <main
         className={`w-screen h-screen inset-0 bg-cover bg-right bg-fixed overflow-auto bg-[url("/background.jpg")] px-4 xl:px-[5%] pt-28 pb-10 flex justify-between gap-5 text-white`}
       >
-        <div className="w-3/5 p-4 overflow-y-auto rounded-xl dark:bg-black/60 bg-white/60 backdrop-blur-lg shadow-[0_0_30px_rgb(0,0,0,0.6)]">
-          <PizzaComponents />
-        </div>
-        <div className="w-1/3 p-4 overflow-y-auto rounded-xl dark:bg-black/60 bg-white/60 backdrop-blur-lg shadow-[0_0_30px_rgb(0,0,0,0.6)]">
-          <PizzaCart />
+        <div className="w-full h-full rounded-xl dark:bg-black/60 bg-white/60 backdrop-blur-lg shadow-[0_0_30px_rgb(0,0,0,0.6)]">
+          <div className="w-3/5 p-4 overflow-y-auto">
+            <PizzaComponents cart={cart} updateCart={setCart} />
+          </div>
+          <div className="w-1/3 p-4 overflow-y-auto ">
+            {/* <PizzaCart cart={cart} /> */}
+          </div>
         </div>
       </main>
     </PageLayout>
