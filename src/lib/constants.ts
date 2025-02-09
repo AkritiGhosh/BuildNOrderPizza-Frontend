9232cce52d2f198e9b3fcff28be1363541907c32
AkritiGhosh <akritighosh98@gmail.com>
2025-02-09 21:01:36 +0530

    multistep form for selecting pizza features

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,11 @@ export const UserProfiles = {
   EMPLOYEE: "employee",
   ADMIN: "admin",
 };
+
+export const pizzaBuildingSteps = [
+  { id: "NUM_PIZZA", title: "Select number of pizzas" },
+  { id: "SIZE", title: "Select pizza size" },
+  { id: "CRUST", title: "Select pizza crust" },
+  { id: "TOPPINGS", title: "Select pizza toppings" },
+  { id: "REVIEW", title: "Review your order" },
+];
