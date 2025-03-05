92e32298b66f49444cbd3e43e1e4b79735aa9ee5
AkritiGhosh <akritighosh98@gmail.com>
2025-03-05 11:33:07 +0530

    integrate slice with UI

--- src/components/user/build-pizza/PizzaComponents.tsx
+++ src/components/user/build-pizza/PizzaComponents.tsx
@@ -4,19 +4,20 @@ import {  useState } from "react";
 // import Stepper from "./multistepPizzaBuilder/Stepper";
 import PizzaAccordion from "../../common/accordion/PizzaAccordion";
 import MultiStepForm from "./multistepPizzaBuilder/MultiStepForm";
+import { useAppSelector } from "../../../hooks/reduxHooks";
 // const OptionCard = lazy(() => import("./OptionCard"));
 // const Accordion = lazy(
 //   () => import("../../common/accordion/ToppingsAccordion")
 // );
 
 const PizzaComponents = () => {
-  const [cart, setCart] = useState([1, 3]);
+  const cart = useAppSelector((state) => state.cart);
 
   return (
     <div className="w-full h-full p-4 overflow-y-auto rounded dark:bg-black/80 bg-white/60 backdrop-blur-2xl shadow-[0_0_30px] shadow-amber-600/30 border-2 border-black/80">
       <h1 className="mb-5 text-3xl text-center font-serif tracking-wider">Build your pizza</h1>
-      {cart?.map((_) => (
-        <PizzaAccordion>
+      {cart?.map((cartData) => (
+        <PizzaAccordion pizzaId={cartData.id} key={cartData.id}>
           <MultiStepForm />
         </PizzaAccordion>
       ))}
