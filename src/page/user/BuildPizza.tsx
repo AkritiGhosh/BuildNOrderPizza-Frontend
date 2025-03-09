39585df82cf8aebe2bfd6f6785a3cf2761a4aa70
AkritiGhosh <akritighosh98@gmail.com>
2025-03-09 16:02:38 +0530

    dark mode bg change

--- src/page/user/BuildPizza.tsx
+++ src/page/user/BuildPizza.tsx
@@ -1,19 +1,14 @@
-import { useState } from "react";
 import PageLayout from "../../components/common/PageLayout";
 import PizzaComponents from "../../components/user/build-pizza/PizzaComponents";
-// import { useState } from "react";
 
 const BuildPizza = () => {
-  const darkMode = document.body.classList.contains("dark");
-  console.log(darkMode);
-  const bgUrl = darkMode
-    ? 'bg-[url("/bg/dark.jpg")]'
-    : 'bg-[url("/bg/light.jpg")]';
   return (
     <PageLayout>
       <main
-        style={{ backgroundSize: "25%" }}
-        className={`relative w-screen h-auto min-h-screen lg:h-screen inset-0 bg-repeat overflow-auto ${bgUrl} px-4 xl:px-[5%] pt-20 lg:pt-28 pb-8 flex flex-col justify-between gap-5 text-white`}
+        style={{
+          backgroundSize: "25%",
+        }}
+        className={`relative w-screen h-auto min-h-screen lg:h-screen inset-0 bg-repeat overflow-auto bg-[url('bg/light.jpg')] dark:bg-[url('bg/dark.jpg')] px-4 xl:px-[5%] pt-20 lg:pt-28 pb-8 flex flex-col justify-between gap-5 text-white`}
       >
         <PizzaComponents />
         <div className="w-full pr-8 md:pr-0 h-10 md:h-14 md:right-4 xl:right-[5%] bottom-4 md:bottom-8 fixed md:absolute flex justify-center md:justify-end">
