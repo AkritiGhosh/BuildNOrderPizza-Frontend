685676d953b25ea309170c59ed77a3e2d9843686
AkritiGhosh <akritighosh98@gmail.com>
2025-03-10 12:22:48 +0530

    Create userSlice.ts

---
+++ src/redux/slice/userSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userId: "",
+  email: "",
+  name: "",
+  address: [],
+  phone: "",
+  orders: [],
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (state, action) => {},
+    registerUser: (state, action) => {},
+    createProfile: (state, action) => {},
+    updateProfile: (state, action) => {},
+    logoutUser: (state, action) => {},
+    getUserOrders: (state, action) => {},
+    getOrderDetails: (state, action) => {},
+    addAddress: (state, action) => {},
+    removeAddress: (state, action) => {},
+    updateAddress: (state, action) => {},
+    deleteAccount: (state, action) => {},
+  },
+});
+export const {
+  loginUser,
+  registerUser,
+  createProfile,
+  updateProfile,
+  logoutUser,
+  getUserOrders,
+  getOrderDetails,
+  addAddress,
+  removeAddress,
+  updateAddress,
+  deleteAccount,
+} = userSlice.actions;
+export default userSlice.reducer;
