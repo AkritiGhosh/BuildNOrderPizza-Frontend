793a9c46b179cde8594da29713094dea997eca83
AkritiGhosh <akritighosh98@gmail.com>
2025-03-13 19:33:54 +0530

    checkout - bg UI

--- src/page/consumer/placeOrder/OrderPage.tsx
+++ src/page/consumer/placeOrder/OrderPage.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import PageLayout from "../../../components/common/PageLayout";
 
 const OrderPage = () => {
   return (
-    <div>OrderPage</div>
-  )
-}
+    <PageLayout className="px-4 xl:px-[15%] pt-20 lg:pt-28 pb-8">
+      <main
+        className={`relative w-screen h-auto min-h-full lg:grow bg-white/85 dark:bg-black/85 overflow-auto p-4 flex flex-col justify-between gap-5`}
+      >
 
-export default OrderPage
+      </main>
+    </PageLayout>
+  );
+};
+
+export default OrderPage;
