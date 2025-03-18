a20827d1fdf556884a7812e587f3bfa986d4bb1f
AkritiGhosh <akritighosh98@gmail.com>
2025-03-19 00:26:25 +0530

    fixed

--- src/hooks/useDialog.tsx
+++ src/hooks/useDialog.tsx
@@ -22,13 +22,17 @@ export const DialogProvider = ({ children }: { children: ReactNode }) => {
   return (
     <DialogContext.Provider value={{ isDialogOpen, closeDialog, openDialog }}>
       {children}
-      <div
-        onClick={closeDialog}
-        className="w-screen h-screen fixed inset-0 bg-amber-100/10 dark:bg-slate-900/10 overflow-hidden z-[9999]"
-      />
-      <section className="bg-amber-100 dark:bg-slate-900 w-[calc(100%-32px)] mx-auto sm:max-w-xs border border-white dark:border-black rounded-md">
-        {dialogContent}
-      </section>
+      {isDialogOpen && (
+        <>
+          <div
+            onClick={closeDialog}
+            className="w-screen h-screen fixed inset-0 bg-amber-100/10 dark:bg-slate-900/10 overflow-hidden z-[9999]"
+          />
+          <section className="bg-amber-100 dark:bg-slate-900 w-[calc(100%-32px)] mx-auto sm:max-w-xs border border-white dark:border-black rounded-md">
+            {dialogContent}
+          </section>
+        </>
+      )}
     </DialogContext.Provider>
   );
 };
