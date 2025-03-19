0a4cc205aa7db62712dc2b0691620d37c2c5f7b7
AkritiGhosh <akritighosh98@gmail.com>
2025-03-19 22:27:33 +0530

    extra component not needed

--- src/page/user/BuildPizza.tsx
+++ src/page/user/BuildPizza.tsx
@@ -1,33 +1,29 @@
+import PizzaAccordion from "../../components/common/accordion/PizzaAccordion";
+import Button from "../../components/common/button/Button";
 import PageLayout from "../../components/common/PageLayout";
-import PizzaComponents from "../../components/user/build-pizza/PizzaComponents";
+import MultiStepForm from "../../components/user/build-pizza/multistepPizzaBuilder/MultiStepForm";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { addNewPizza } from "../../redux/slice/cartSlice";
 
 const BuildPizza = () => {
+  const cart = useAppSelector((state) => state.cart);
+  const dispatch = useAppDispatch();
+  const handleAdd = () => dispatch(addNewPizza());
+
   return (
     <PageLayout>
-      <main
-        className={`relative w-screen h-auto min-h-screen lg:h-screen inset-0 bg-repeat overflow-auto px-4 xl:px-[5%] pt-20 lg:pt-28 pb-8 flex flex-col justify-between gap-5 text-white`}
-      >
-        <PizzaComponents />
-        <div className="w-full pr-8 md:pr-0 h-10 md:h-14 md:right-4 xl:right-[5%] bottom-4 md:bottom-8 fixed md:absolute flex justify-center md:justify-end">
-          <button className="w-full md:w-14 h-10 md:h-14 bg-amber-700 rounded md:rounded-full flex items-center justify-center gap-2">
-            <span className="inline-block md:hidden">View Cart</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="size-6 md:size-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
-          </button>
-        </div>
-      </main>
+      <h1 className="mb-5 text-5xl text-amber-950 dark:text-slate-300 text-center font-cursive font-medium tracking-wider">
+        Build your pizza
+      </h1>
+      {cart?.map((cartData) => (
+        <PizzaAccordion pizzaId={cartData.id} key={cartData.id}>
+          <MultiStepForm pizzaId={cartData.id} />
+        </PizzaAccordion>
+      ))}
+      <Button onClick={handleAdd} type="filled">
+        Add another pizza
+      </Button>
+     
     </PageLayout>
   );
 };
