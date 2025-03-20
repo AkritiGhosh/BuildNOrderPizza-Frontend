a3394f4e9235e19aac24445cc3bc0759b5855bcd
AkritiGhosh <akritighosh98@gmail.com>
2025-03-20 10:59:23 +0530

    add route for cart

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { RouteObject } from "react-router-dom";
-import BuildPizza from "../page/user/BuildPizza";
+const BuildPizza = lazy(() => import("../page/user/BuildPizza"));
+const CartPage = lazy(() => import("../page/user/CartPage"));
 const PageNotFound = lazy(() => import("../page/user/404"));
 const AuthPage = lazy(() => import("../page/user/AuthPage"));
 const LandingPage = lazy(() => import("../page/user/LandingPage"));
@@ -12,6 +13,10 @@ const PublicRoutes = (): RouteObject[] => {
     {
       path: "/build-your-pizza",
       Component: BuildPizza,
+    },
+    {
+      path: "/cart",
+      Component: CartPage,
     },
     { path: "*", Component: PageNotFound },
   ];
