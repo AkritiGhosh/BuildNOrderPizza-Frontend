15e9efdc26db1e7e85ced14952b35451fba84bdc
AkritiGhosh <akritighosh98@gmail.com>
2025-03-20 10:58:46 +0530

    Create CartPage.tsx

---
+++ src/page/user/CartPage.tsx
@@ -0,0 +1,36 @@
+import PageLayout from "../../components/common/PageLayout";
+import Heading from "../../components/common/Heading";
+import { useNavigate } from "react-router-dom";
+
+const CartPage = () => {
+  const navigate = useNavigate();
+  return (
+    <PageLayout>
+      <nav>
+        <button
+          onClick={() => navigate(-1)}
+          className="inline-flex w-auto gap-1 items-center py-0.5 px-1.5 rounded-md hover:bg-amber-100 dark:hover:bg-slate-800 text-amber-600 dark:text-slate-300 text-base hover:font-semibold"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          Back
+        </button>
+      </nav>
+      <Heading>Cart</Heading>
+    </PageLayout>
+  );
+};
+
+export default CartPage;
