ce963cf320460871608ca214651a4b117ffd95e4
AkritiGhosh <akritighosh98@gmail.com>
2025-03-20 10:58:21 +0530

    Create Heading.tsx

---
+++ src/components/common/Heading.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+const Heading = ({ children }: { children: ReactElement | string }) => {
+  return (
+    <h1 className="mb-5 text-5xl text-amber-950 dark:text-slate-300 text-center font-cursive font-medium tracking-wider">
+      {children}
+    </h1>
+  );
+};
+
+export default Heading;
