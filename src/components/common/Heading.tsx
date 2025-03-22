dd47a203ec483a994946cfe412d452754c4b4e7d
AkritiGhosh <akritighosh98@gmail.com>
2025-03-22 10:40:12 +0530

    center align - fixed

--- src/components/common/Heading.tsx
+++ src/components/common/Heading.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react";
 
 const Heading = ({ children }: { children: ReactElement | string }) => {
   return (
-    <h1 className="mb-5 text-5xl text-amber-950 dark:text-slate-300 text-center font-cursive font-medium tracking-wider">
+    <h1 className="w-full mb-5 text-5xl text-amber-950 dark:text-slate-300 text-center font-cursive font-medium tracking-wider">
       {children}
     </h1>
   );
