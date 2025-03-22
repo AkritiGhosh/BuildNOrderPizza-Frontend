14bd69b1f85708b902bf75a0e6e566181693d7d0
AkritiGhosh <akritighosh98@gmail.com>
2025-03-23 00:51:48 +0530

    App routes updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Suspense } from "react";
-import { checkAuth } from "./lib/core";
-// import { UserContext } from "./lib/context";
-import PrivateRoutes from "./routes/PrivateRoutes";
-import PublicRoutes from "./routes/PublicRoutes";
 import Loader from "./components/user/Loader";
 import { ThemeProvider } from "./hooks/useTheme";
 import { DialogProvider } from "./hooks/useDialog";
+import Routes from "./routes/Routes";
 
 function App() {
-  const privateRouter = createBrowserRouter([
-    ...PrivateRoutes(),
-    ...PublicRoutes(),
-  ]);
-  const publicRouter = createBrowserRouter(PublicRoutes());
+ 
+  const router = createBrowserRouter(Routes)
 
   return (
     <Suspense fallback={<Loader />}>
       <DialogProvider>
         <ThemeProvider>
-          <RouterProvider router={checkAuth() ? privateRouter : publicRouter} />
+          <RouterProvider router={router} />
         </ThemeProvider>
       </DialogProvider>
     </Suspense>
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,48 @@
+import { lazy } from "react";
+import { Outlet, RouteObject } from "react-router-dom";
+const BuildPizza = lazy(() => import("../page/user/BuildPizza"));
+const CartPage = lazy(() => import("../page/user/CartPage"));
+const PageNotFound = lazy(() => import("../page/user/404"));
+const AuthPage = lazy(() => import("../page/user/AuthPage"));
+const LandingPage = lazy(() => import("../page/user/LandingPage"));
+
+const ProfileForm = lazy(() => import("../page/consumer/ProfileForm"));
+const Profile = lazy(() => import("../page/consumer/Profile"));
+const Orders = lazy(() => import("../page/consumer/Orders"));
+const Settings = lazy(() => import("../page/consumer/Settings"));
+const OrderPage = lazy(() => import("../page/consumer/placeOrder/OrderPage"));
+
+const AuthenticatedRoutes = () => {
+  const isAuthenticated = true;
+  if (!isAuthenticated) return null;
+  return <Outlet />;
+};
+const Routes: RouteObject[] = [
+  { path: "/", element: <LandingPage /> },
+  { path: "/auth", element: <AuthPage /> },
+  { path: "/build-your-pizza", element: <BuildPizza /> },
+  { path: "/cart", element: <CartPage /> },
+
+  {
+    element: <AuthenticatedRoutes />,
+    children: [
+      { path: "/create-profile", element: <ProfileForm /> },
+      {
+        path: "profile/:profileId",
+        children: [
+          { path: "edit-profile", element: <ProfileForm /> },
+          { path: "profile", element: <Profile /> },
+          { path: "orders", element: <Orders /> },
+          { path: "orders/:orderId", element: <Orders /> },
+        ],
+      },
+      { path: "/settings", element: <Settings /> },
+      { path: "/checkout", element: <OrderPage /> },
+    ],
+  },
+
+  // ✅ Ensure a catch-all wildcard at the very bottom
+  { path: "*", element: <PageNotFound /> },
+];
+
+export default Routes;
