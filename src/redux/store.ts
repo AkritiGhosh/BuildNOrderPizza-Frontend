8794eed4b2644a85f6c810a90d563e905dfcb832
AkritiGhosh <akritighosh98@gmail.com>
2025-03-24 20:48:36 +0530

    store updated for apis

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slice/cartSlice";
+import { apiSlice } from "./slice/apiSlice";
 
 const appReducer = combineReducers({
   cart: cartReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
 });
 
 const store = configureStore({
   reducer: appReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
